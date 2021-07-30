c8656d2f598b0bb91e32547d7a2bedb88810fb38
unknown <ugiut@student.kit.edu>
2021-07-30 16:53:04 +0200

    adapt to official data source

---
+++ code/auto_download/MZ_download.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 18:37:07 2021
+
+@author: Jannik
+"""
+
+import urllib
+import glob
+from unidecode import unidecode
+import os
+import zipfile
+import pandas as pd
+import datetime
+    
+# look up abbreviations of voivodships
+
+abbr_vois = {"slaskie": "PL83", "mazowieckie": "PL78", "malopolskie": "PL77", 
+             "wielkopolskie": "PL86", "lodzkie": "PL74", "dolnoslaskie": "PL72", 
+             "pomorskie": "PL82", "podkarpackie": "PL80",
+             "kujawsko-pomorskie": "PL73", "lubelskie": "PL75", 
+             "opolskie": "PL79", "swietokrzyskie": "PL84", "podlaskie": "PL81",
+             "zachodniopomorskie": "PL87", "warminsko-mazurskie": "PL85", 
+             "lubuskie": "PL76", "Caly kraj": "PL"}
+
+abbr_vois_omnious_encoding = {"lskie": "PL83", "mazowieckie": "PL78", "maopolskie": "PL77", 
+             "wielkopolskie": "PL86", "dzkie": "PL74", "dolnolskie": "PL72", 
+             "pomorskie": "PL82", "podkarpackie": "PL80",
+             "kujawsko-pomorskie": "PL73", "lubelskie": "PL75", 
+             "opolskie": "PL79", "witokrzyskie": "PL84", "podlaskie": "PL81",
+             "zachodniopomorskie": "PL87", "warmisko-mazurskie": "PL85", 
+             "lubuskie": "PL76", "Cay kraj": "PL"}
+
+# due to unknown encoding we have to map the names back to real names
+map_abbr_name = {"PL72": "Dolnoslaskie", "PL73": "Kujawsko-Pomorskie",
+                 "PL75": "Lubelskie", "PL76": "Lubuskie", "PL74": "Lodzkie",
+                 "PL77": "Malopolskie", "PL78": "Mazowieckie",
+                 "PL79": "Opolskie", "PL80": "Podkarpackie", "PL81": "Podlaskie",
+                 "PL82": "Pomorskie", "PL83": "Slaskie", "PL84": "Swietokrzyskie",
+                 "PL85": "Warminsko-Mazurskie", "PL86": "Wielkopolskie",
+                 "PL87": "Zachodniopomorskie", "PL": "Poland"}
+
+# download raw zip file
+#urllib.request.urlretrieve("https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data", "poland.zip")
+#
+# extract file
+#file_name = os.path.abspath("poland.zip")
+#zip_ref = zipfile.ZipFile(file_name)
+#os.mkdir(os.path.join(os.getcwd(), "poland_unzip"))
+#zip_ref.extractall("poland_unzip")
+
+inc_case_dfs = []
+inc_death_dfs = []
+
+curr_inc_case = pd.read_csv("../../data-truth/MZ/truth_MZ-Incident Cases_Poland.csv")
+curr_inc_deaths = pd.read_csv("../../data-truth/MZ/truth_MZ-Incident Deaths_Poland.csv")
+
+curr_cum_case = pd.read_csv("../../data-truth/MZ/truth_MZ-Cumulative Cases_Poland.csv")
+curr_cum_deaths = pd.read_csv("../../data-truth/MZ/truth_MZ-Cumulative Deaths_Poland.csv")
+
+# get csv files
+for file in os.listdir("./poland_unzip"):
+    if file.endswith(".csv"):
+        df = pd.read_csv(os.path.join(os.path.join(os.getcwd(), "poland_unzip"), file), sep=";", engine='python')
+        df["wojewodztwo"] = df["wojewodztwo"].apply(lambda x: unidecode(x))
+        try:
+            df["location"] = df["wojewodztwo"].apply(lambda x: abbr_vois[x])
+        except:
+            # if the above fails, the encoding is weird and we have to process the files separately
+            df["location"] = df["wojewodztwo"].apply(lambda x: abbr_vois_omnious_encoding[x])
+        
+        df["location_name"] = df["location"].apply(lambda x: map_abbr_name[x])
+        
+        #extract date from filename
+        date = file[0:8]
+        df["date"] = (date[0:4] + "." + date[4:6] + "." + date[6:])
+        df["date"] = pd.to_datetime(df["date"], format="%Y.%m.%d")
+        #shift to ecdc
+        df["date"] = df["date"]. apply(lambda x: x + datetime.timedelta(days=1))
+        
+        inc_case_df = df[["date", "location_name", "location", "liczba_przypadkow"]].rename(columns={"liczba_przypadkow": "value"})
+        inc_case_dfs.append(inc_case_df)
+        
+        inc_death_df = df[["date", "location_name", "location", "zgony"]].rename(columns={"zgony": "value"})
+        inc_death_dfs.append(inc_death_df)
+
+inc_case_df = pd.concat(inc_case_dfs)
+inc_death_df = pd.concat(inc_death_dfs)
+
+# only use new data
+last_update = curr_inc_case["date"].max()
+
+# cut off dates
+inc_case_df = inc_case_df[inc_case_df["date"] > last_update]
+inc_death_df = inc_death_df[inc_death_df["date"] > last_update]
+inc_case_df["date"] = inc_case_df["date"].dt.date
+inc_death_df["date"] = inc_death_df["date"].dt.date
+
+#create cumulatve data
+cum_case_df = inc_case_df.copy()
+cum_case_df["value"] = cum_case_df.groupby("location").cumsum()
+
+cum_death_df = inc_death_df.copy()
+cum_death_df["value"] = cum_death_df.groupby("location").cumsum()
+
+# add new data to dataframe
+final_inc_case = pd.concat([curr_inc_case, inc_case_df])
+final_inc_case = final_inc_case.set_index("date")
+
+final_inc_deaths = pd.concat([curr_inc_deaths, inc_death_df])
+final_inc_deaths = final_inc_deaths.set_index("date")
+
+# create cum data
+latest_death_sum = curr_cum_deaths[curr_cum_deaths["date"] == curr_cum_deaths["date"].max()][["location", "value"]]
+latest_death_sum = pd.Series(latest_death_sum.value.values, index=latest_death_sum.location).to_dict()
+
+latest_case_sum = curr_cum_case[curr_cum_case["date"] == curr_cum_case["date"].max()][["location", "value"]]
+latest_case_sum = pd.Series(latest_case_sum.value.values, index=latest_case_sum.location).to_dict()
+
+def update_cumulative(location, value, previous_cumulative):
+    return value + previous_cumulative[location]
+
+cum_death_df["value"] = cum_death_df.apply(lambda row: update_cumulative(row["location"], row["value"], latest_death_sum), axis=1)
